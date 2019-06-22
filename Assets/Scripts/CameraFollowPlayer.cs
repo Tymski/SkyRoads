09f2adeb498c7a82a1c922c4b105ad4d478d25c1
Tymski <Tymon.Oziemblewski@gmail.com>
2019-06-23 00:25:06 +0200

    first commit

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+    public Transform playerTransform;
+    private void FixedUpdate () {
+        transform.position = new Vector3 (
+            transform.position.x,
+            transform.position.y,
+            //playerTransform.position.z - 15
+            Mathf.Lerp (playerTransform.position.z - 15, playerTransform.position.z - 15, .3f)
+        );
+    }
+}
